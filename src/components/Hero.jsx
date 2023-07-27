60ee32142860b67a3f60a48e5bcac724538f2370
deeMswazi <95373816+deemswazi@users.noreply.github.com>
2023-07-27 10:25:30 +0300

    setting up autoprefixer dependancy

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Image} from './images/bgImg.jpg'
+
+
+const Hero = () => {
+    return (
+        <div>
+           <img src={"./images/bgImg.jpg"} alt="Background" />
+
+        </div>
+    )
+}
+export default Hero
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('taiwindcss'),
+    require('autoprefixer'),
+  ],
 }
 
